a960c8bc3da9ad035a85395d596903c72dbc69cf
Issac Davis <trksprkz@gmail.com>
2022-07-10 04:12:51 -0700

    updated, deleted old repository as it wouldnt update

---
+++ src/components/WorkData.js
@@ -0,0 +1,49 @@
+export const Work = [
+    {
+        id:1,
+        name:"Fullstack Amazon Clone",
+        description:"Built with React for front end, Node for backend, and Firebase for authentication. Check it out ↓",
+        tags:["react","node","firebase","mongodb"],
+
+        demo:"https://incredible-sfogliatella-087438.netlify.app",
+        github:"https://github.com/trksprkz"
+    },
+    {
+        id:2,
+        name:"Restaurant Ordering",
+        description:"Web Application for ordering food using React and Node. Check it out ↓",
+        tags:["react","firebase","node"],
+
+        demo:"https://stunning-kataifi-338ab4.netlify.app",
+        github:"https://github.com/trksprkz/foodDelivery"
+
+    },
+    {
+        id:3,
+        name:"Password Generator",
+        description:"For those times your '87' passwords don't fit a website criteria. We've all been there...",
+        tags:["react","api","fetch","algorithms"],
+
+        demo:"https://graceful-pie-2fb3bc.netlify.app",
+        github:"https://github.com/trksprkz/passwordGen"
+    },
+    {
+        id:4,
+        name:"Simple Movie Database",
+        description:"A movie (and game) database. Go search for a movie!",
+        tags:["react","styledComponents","react-router","react hooks"],
+
+        demo:"https://trksprkz.github.io/reactMovieDatabase/",
+        github:"https://github.com/trksprkz/reactMovieDatabase"
+
+    },{
+        id:5,
+        name:"SafeTravels",
+        description:"A huge Fullstack Web App made for users to sign up, and add a travel route. Finding camping spots along the way, allows for peer reviews and comments.",
+        tags:["express","cloudinary","helmet","google api","node","mongodb",],
+
+        demo:"https://github.com/trksprkz/safeTravels",
+        github:"https://github.com/trksprkz/safeTravels"
+
+    },
+]
